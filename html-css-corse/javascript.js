3692d9fbc7151e17abe24ad10b60131ce056bb23
PavanKalyan99 <danireddypavankalyan@gmail.com>
2026-03-16 21:10:10 +0530

    initial commit

---
+++ html-css-corse/javascript.js
@@ -0,0 +1,9 @@
+<html>
+  <body> 
+    <h1>now we discussing how to change the html content</h1>
+    <h2 id="hcontent">my name is pavan kalyan</h2>
+    <h3>welcome to javascript</h3>
+    <button type="button" onclick='document.getElementById("hcontent").innerHTML="hello pavan"'>pavan</button>
+    <button type="button" onclick='document.getElementById("hcontent").innerHTML="hello kalyan"'>kalyan</button>
+  </body>
+</html>
